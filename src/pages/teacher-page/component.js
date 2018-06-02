8cdcd71a706c0262fd57b52a3f6b958fc5239284
Adam Alcott <aalcott14@gmail.com>
2018-06-02 15:01:08 -0500

    add teacher login form with redirect

--- src/pages/teacher-page/component.js
+++ src/pages/teacher-page/component.js
@@ -1,16 +1,43 @@
 import * as React from 'react';
+import { Redirect } from 'react-router';
+import { InputGroup, Icon, ControlGroup, Button} from '@blueprintjs/core';
 
-import { generateRandomName } from '../../utils/randomNameGenerator';
+import '@blueprintjs/core/lib/css/blueprint.css';
 
 export class TeacherPage extends React.Component {
+  state = {
+    teacherID: '',
+  }
   render() {
     return (
       <div>
         <header>
-          <h1>{generateRandomName()}</h1>
+          Welcome
         </header>
-        <div>Teacher Page</div>
+        <div>
+          <ControlGroup round>
+            <InputGroup
+              large
+              value={this.state.teacherID}
+              placeholder="Enter teacher ID"
+              type="text"
+              onChange={(e) => this._handleIdChange(e.target.value)}
+            />
+            <Button onClick={() => this._loginTeacher()} icon="arrow-right" type="submit"></Button>
+          </ControlGroup>
+        </div>
       </div>
     );
+  }
+
+  _handleIdChange = (val) => {
+    this.setState({
+      teacherID: val,
+    })
+  }
+
+  _loginTeacher = () => {
+    console.log('trying', this.state.teacherID)
+    window.location = `teacher/${this.state.teacherID}`;
   }
 }
