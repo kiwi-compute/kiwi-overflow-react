0c02e485bf3392b0c66fb77802f2fe9831395594
Adam Alcott <aalcott14@gmail.com>
2018-06-02 12:29:40 -0500

    firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyBwHwxUc8W0ZKViBdBHXla1oqW_y6pUOCE",
+  authDomain: "stinky-feet.firebaseapp.com",
+  databaseURL: "https://stinky-feet.firebaseio.com",
+  projectId: "stinky-feet",
+  storageBucket: "",
+  messagingSenderId: "188025015830"
+};
