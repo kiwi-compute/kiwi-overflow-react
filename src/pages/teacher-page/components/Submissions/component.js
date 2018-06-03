0bde728bcda35c9f81eb84f45904407f496fea93
Ceci <ceci.goss@gmail.com>
2018-06-03 12:22:29 -0500

    add submissions page... styling needs to be fixed majorly

--- src/pages/teacher-page/components/Submissions/component.js
+++ src/pages/teacher-page/components/Submissions/component.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-class Submissions extends React.Component {
+import { withRouter } from 'react-router';
+import { Button } from '@blueprintjs/core';
+
+import './styles.css';
+
+class SubmissionsComponent extends React.Component {
   render() {
     const question =
       'Bacon ipsum dolor amet pancetta frankfurter shankle pork chop t-bone hamburger venison turducken andouille bacon sirloin ground round. Beef ribs tri-tip tenderloin bacon shankle biltong pastrami swine leberkas burgdoggen chuck brisket picanha chicken. Picanha venison biltong turkey, capicola spare ribs shank doner meatloaf.'; // this.props.question;
@@ -12,10 +17,14 @@ class Submissions extends React.Component {
       // TODO: Perhaps turn the submission element below into a separate component if needed?
       return (
         <div className="submission">
-          <img
-            className="icon"
-            src="https://png.icons8.com/color/1600/person-male.png"
-          />
+          <div className="avatar">
+            <img
+              width="50px"
+              height="50px"
+              className="icon"
+              src="https://png.icons8.com/color/1600/person-male.png"
+            />
+          </div>
           <div className="answer">
             Chicken kielbasa corned beef meatloaf meatball, chuck frankfurter
             turkey short loin. Rump meatloaf corned beef beef ribs meatball cow
@@ -28,15 +37,30 @@ class Submissions extends React.Component {
 
     return (
       <div className="submissions-page">
-        <div className="question">
-          <h3>{questionNumber}</h3>
-          <p>{question}</p>
+        <div className="container">
+          <div className="question">
+            <h3>Question {questionNumber}</h3>
+            <p>{question}</p>
+          </div>
+          <div className="submissions">
+            <h5>ANSWERS</h5>
+            <div className="entries">{submissions}</div>
+          </div>
         </div>
-        <div className="submissions">
-          <h3>Answers</h3>
-          <div className="entries">{submissions}</div>
+        <div className="footer">
+          <Button
+            type="button"
+            className="studentButton pt-button"
+            onClick={() => {
+              console.log('button pressed');
+            }}
+          >
+            CT
+          </Button>
         </div>
       </div>
     );
   }
 }
+
+export const SubmissionsPage = withRouter(SubmissionsComponent);
