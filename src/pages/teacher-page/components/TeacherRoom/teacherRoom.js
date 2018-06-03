2c268beea5bf6ba069d9c3b38a06d4aa10265b17
Adam Alcott <aalcott14@gmail.com>
2018-06-03 10:40:55 -0500

    renders students and subscribes to step

--- src/pages/teacher-page/components/TeacherRoom/teacherRoom.js
+++ src/pages/teacher-page/components/TeacherRoom/teacherRoom.js
@@ -2,44 +2,58 @@ import React from 'react';
 
 import './teacherRoom.css';
 import { getRoomByName } from '../../../../api/get-room';
+import { subscribeToRoomByID } from '../../../../api/subscribe-to-room';
 import NavbarComponent from '../Navbar/navbar';
-import { Spinner } from '@blueprintjs/core';
+import { SVGSpinner, Card, Elevation, Button } from '@blueprintjs/core';
+import { transitionStep } from '../../../../api/transition-step';
 
 //TODO: Rename to 'lobby'
 
 export class TeacherRoom extends React.Component {
   state = {
     room: null,
+    students: [],
+    step: null,
   }
 
   componentDidMount() {
     getRoomByName(this.props.match.params.roomName).then((room) => {
-      console.log('room', room);
       this.setState({
         room,
+      })
+      subscribeToRoomByID(room.id, (room) => {
+        console.log('room', room);
+        if (room.students) {
+          this.setState({
+            students: room.students,
+          })
+        }
       })
     })
   }
 
   render() {
     return (
-      this.state.room ? (
-        <React.Fragment>
-          <NavbarComponent authenticated />
+      <div className="wrapper">
+         <NavbarComponent authenticated />
+        {this.state.room ? (
           <div className="teacher-room-info">
-            <div className="url">
-              <div className="section">
-                URL
-              </div>
+            <div className="card">
+              <Card elevation={Elevation.TWO}>
+                <h1>URL:</h1>
+                <p>{`/${this.props.match.params.roomName}`}</p>
+              </Card>
             </div>
-            <div className="student-count">
-              <div className="section">
-                Number of students            
-              </div>
+            <div className="card">
+              <Card elevation={Elevation.TWO}>
+                <h1>Number of students:</h1>
+                <p>{`${this.state.students.length}`}</p>
+              </Card>
             </div>
           </div>
-        </React.Fragment>
-      ) : (<Spinner large />)
+      ) : (<SVGSpinner />)}
+      <Button onClick={() => transitionStep(1, this.state.room.id)}>Start Game</Button>
+      </div>
     );
   }
 }
